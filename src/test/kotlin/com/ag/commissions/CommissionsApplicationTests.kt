7ce3e9634949d9545855954ddf487bdeae6d9a53
agrazhdankin <QzEJAAW2aPGPej9J>
2022-03-08 21:00:24 +0200

    Initial commit

---
+++ src/test/kotlin/com/ag/commissions/CommissionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ag.commissions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommissionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
